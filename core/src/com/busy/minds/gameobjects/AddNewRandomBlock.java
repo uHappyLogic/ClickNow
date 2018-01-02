48aa04517bd2bcad25d96c99f89be1908c5b4c8e
JN <janekpro996@gmail.com>
2018-01-02 16:21:04 +0100

    RandomBlock

--- core/src/com/busy/minds/gameobjects/AddNewRandomBlock.java
+++ core/src/com/busy/minds/gameobjects/AddNewRandomBlock.java
@@ -1,6 +1,10 @@
 package com.busy.minds.gameobjects;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
 
 /**
  * Created by Jan on 28.12.2017.
@@ -13,15 +17,30 @@ public class AddNewRandomBlock implements IGameAction{
     }
     @Override
     public void Execute() {
-        Random random = new Random();
-        if(random.nextFloat()>0.6f){
-            blockManager.AddGreenBlock();
-        }else
-        if(random.nextFloat()<0.3f){
-            blockManager.AddRedBlock();
-        }else{
-            blockManager.AddPurpleBlock();
-        }
+        List <String>availableBlocks=new ArrayList<>();
+        availableBlocks.add("green");
+        availableBlocks.add("purple");
+        availableBlocks.add("red");
+        Collections.shuffle(availableBlocks);
+       switch (availableBlocks.get(0)){
+           case "green":blockManager.AddGreenBlock();
+                break;
+           case "purple":blockManager.AddPurpleBlock();
+               break;
+           case "red":blockManager.AddRedBlock();
+               break;
+
+       }
+
+//        Random random = new Random();
+//        if(random.nextFloat()>0.6f){
+//            blockManager.AddGreenBlock();
+//        }else
+//        if(random.nextFloat()<0.3f){
+//            blockManager.AddRedBlock();
+//        }else{
+//            blockManager.AddPurpleBlock();
+//        }
     }
     IBlockManager blockManager;
 }
